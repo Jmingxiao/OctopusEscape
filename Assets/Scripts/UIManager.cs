205fa96ab5b3154e0a4e5b354e167031ac44d512
emiltervo <73493921+emiltervo@users.noreply.github.com>
2024-03-01 01:46:39 +0100

    Tried to compile have several errors
    Sample on the Level screen
    https://imgur.com/a/Jh9Yshl

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -57,10 +57,7 @@ public class UIManager : MonoBehaviour
     }
     public void Quit()
     {
-         #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-         #else
         Application.Quit();
-        #endif
+        Debug.Log("Game is exiting");
     }
 }
