5fc6545614eb3fc4a491a9be161ac8b9dae335c8
emiltervo <73493921+emiltervo@users.noreply.github.com>
2024-03-01 01:22:41 +0100

    Modified Menu, Added Level scene
    Modified Menu the menu items.
    Added Level scene to choose the levels.
    Todo: Make Level 1 and 2.

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance { get; private set; }
+    private void Awake() 
+    { 
+    // If there is an instance, and it's not me, delete myself.
+    
+        if (Instance != null && Instance != this) 
+        { 
+            Destroy(this); 
+        } 
+        else 
+        { 
+            Instance = this; 
+        } 
+    }
+    private void OnEnable() {
+        Time.timeScale = 0;
+    }
+
+    private void OnDisable() {
+        Time.timeScale = 1;
+    }
+    public void ResumeGame()
+    {
+       gameObject.SetActive(false);
+    }
+
+    public void StartNewGame()
+    {
+        GameManager.Instance.LoadScene("Tutorial");
+    }
+    public void level1()
+    {
+        GameManager.Instance.LoadScene("level1");
+    }
+    public void level2()
+    {
+        GameManager.Instance.LoadScene("level2");
+    }
+    public void Tutorial()
+    {
+        GameManager.Instance.LoadScene("level2");
+    }
+    public void LoadLevelMenu()
+    {
+        GameManager.Instance.LoadScene("ChoseLevel");
+    }
+
+    public void LoadMainMenu()
+    {
+        GameManager.Instance.LoadScene("MainMenu");
+    }
+    public void Quit()
+    {
+         #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+         #else
+        Application.Quit();
+        #endif
+    }
+}
